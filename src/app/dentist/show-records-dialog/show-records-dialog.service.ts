8c379152a713c633cb26b083ab75c38d2b237e9b
Chris Hamilton <chris_h_27@hotmail.ca>
2022-03-28 11:41:38 -0400

    Add loading notification for record dialog

--- src/app/dentist/show-records-dialog/show-records-dialog.service.ts
+++ src/app/dentist/show-records-dialog/show-records-dialog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { finalize, Observable, Subject } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
+import { LoadingNotificationService } from 'src/app/services/loading-notification.service';
 import { IPatient } from 'src/schema/person';
 import { Treatment } from 'src/schema/treatment';
 
@@ -14,14 +15,20 @@ export class ShowRecordsDialogService {
 
   set patient(value: IPatient | undefined) {
     this._patient = value;
-    if (this._patient)
-      this.records = this.bes.getPatientTreatments(this._patient.patient_id);
-    else this.records = new Observable<Treatment[]>();
+    if (this._patient) {
+      this.lns.show();
+      this.records = this.bes
+        .getPatientTreatments(this._patient.patient_id)
+        .pipe(finalize(() => this.lns.hide()));
+    } else this.records = new Observable<Treatment[]>();
   }
 
   get patient() {
     return this._patient;
   }
 
-  constructor(private bes: BackendService) {}
+  constructor(
+    private bes: BackendService,
+    private lns: LoadingNotificationService
+  ) {}
 }
